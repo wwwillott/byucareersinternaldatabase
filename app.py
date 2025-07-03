9c48f38196545d9c7726ef918e491bf038046bc6
wwwillott <williamvott@gmail.com>
2025-07-03 16:43:36 -0600

    moving to render for live hosting

--- app.py
+++ app.py
@@ -374,4 +374,6 @@ def inject_background_image():
     return dict(background_image=get_season_background())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
